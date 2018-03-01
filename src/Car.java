a691b2ef7d718860debd8089380cc2d9c5d5a8c7
unknown <MStanoevich@gmail.com>
2018-03-01 21:42:30 +0100

    Car constructer added and car object generated from input file

--- src/Car.java
+++ src/Car.java
@@ -1,8 +1,15 @@
 
 public class Car {
-	public int[][] posCar = {{0},{0}};
+
+	public int[] posCar;
 	public int carId;
 	public int distance;
 	public Ride[] ridesTaken;
 
+	public Car(int[] posCar, int carId, int distance, Ride[] ridesTaken){
+	    this.posCar = posCar;
+	    this.carId = carId;
+	    this.distance = distance;
+	    this.ridesTaken = ridesTaken;
+    }
 }
